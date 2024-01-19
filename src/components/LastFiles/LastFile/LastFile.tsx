13bf5e0220fccdedc05b442b75868c8bf425e785
Obirah <d.przenioslo2502@gmail.com>
2024-01-19 13:53:31 +0100

    details from dashboard

--- src/components/LastFiles/LastFile/LastFile.tsx
+++ src/components/LastFiles/LastFile/LastFile.tsx
@@ -47,7 +47,8 @@ const LastFile: FC<LastFileProps> = ({file, fileProps}) => {
                     </span>
                 </Card.Text>
                 <div className={`d-flex w-100 justify-content-center`}>
-                    <Button variant="primary" onClick={() => navigate(`/file/${file.fileId}`)}>Check Details</Button>
+                    <Button variant="primary" onClick={() => {localStorage.setItem("JWT_FILE_ID", file.fileId);
+                                                                    navigate(`/file/${file.fileId}`)}}>Check Details</Button>
                 </div>
             </Card.Body>
         </Card>
